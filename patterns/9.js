c2ac40f4e99ec05d0a94cde8afb99bbe86a4f54c
Shivanshu <ks7876555@gmail.com>
2024-01-03 12:31:14 +0530

    Do diamond pattern

--- patterns/9.js
+++ patterns/9.js
@@ -1,1 +1,41 @@
-// to be continued...
+//      *
+//     ***
+//    *****
+//   *******
+//   *******
+//    *****
+//     ***
+//      *
+
+
+const  n = 4
+
+let res = ''
+
+for (let i = 0 ; i < n; i++) {
+    // space 
+    for (let j = 0; j < n-i-1; j ++) {
+        res += ' '
+    }
+    // star 
+    for (let j = 0; j < 2*i +1 ; j++ ) {
+        res += '*'
+    }
+    res += '\n'
+}
+
+
+for (let i = n; i > 0; i--) {
+    // space 
+    for ( let j = 0 ; j < n-i; j++) {
+        res += ' '
+    }
+    //star 
+    for (let j = 0 ; j < i*2 -1; j ++) {
+        res += '*'
+    }
+    res += '\n'
+}
+
+
+console.log(res);
