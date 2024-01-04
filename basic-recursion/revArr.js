bea7e2c1c06004a90dcd61fb77948bb5e9fa80a7
Shivanshu <ks7876555@gmail.com>
2024-01-04 13:26:42 +0530

    do reverse array using recursion

--- basic-recursion/revArr.js
+++ basic-recursion/revArr.js
@@ -1,4 +1,12 @@
-const arr = [5, 4, 3, 2, 1];
-const revArr = (array) => {
-    let len = array.length;
+const arr = [1, 2, 3, 4, 5, 6];
+let len = arr.length - 1;
+let newArr = [];
+const revArr = (array, len) => {
+    if (len >= 0) {
+        newArr.push(array[len]);
+        len--;
+        revArr(array, len);
+    }
 };
+revArr(arr, len);
+console.log(newArr);
