1ad19c349b8ad2f064e9ffc401c7b2ce70d6cf9f
Shivanshu <ks7876555@gmail.com>
2024-01-10 20:58:58 +0530

    revise insertion

--- imp-sorting-techniques/sorting-1/insertion.js
+++ imp-sorting-techniques/sorting-1/insertion.js
@@ -1,15 +1,12 @@
 const arr = [13, 8, 24, 52, 20, 9];
-const n = arr.length;
 
-for (let i = 1; i < n; i++) {
-    let curr = arr[i];
+for (let i = 1; i < arr.length; i++) {
     let j = i - 1;
-
+    let curr = arr[i];
     while (j >= 0 && arr[j] > curr) {
         arr[j + 1] = arr[j];
         j--;
     }
     arr[j + 1] = curr;
 }
-
 console.log(arr);
